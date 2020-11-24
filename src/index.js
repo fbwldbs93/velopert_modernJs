8014f517042f45f0310dc01f4352bb3bc8c4d051
fbwldbs93 <fbwldbs93@naver.com>
2020-11-24 14:34:47 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+/*----------------- 
+02. 변수
+------------------*/
+//변수
+/*
+  변수는 바뀔 수 있는 값
+*/
+let value = 1;
+console.log(value);
+value = 2;
+console.log(value);
+
+//상수
+/*
+  상수는 한번 선언하고 값이 바뀌지
+  않는 값을 의미
+*/
+const a = 1;
+
+let text = "hello";
+let name = "좌봐스크립트";
+
+let good = true;
+let loading = false;
+
+const friend = null;
+
+let criminal;
+console.log(criminal); //undefined
+
+/*
+  null과 undefined 둘 다 값이 없음을
+  의미하는 것은 맞는데,
+
+  null은 우리가 없다고, 고의적으로 
+  설정하는 값을 의미하고,
+  undefined 는 우리가 설정을 하지
+  않았기 때문에 없는 값을 의미
+*/
+
+/*----------------- 
+03. 연산자
+------------------*/
